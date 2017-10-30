585c90f0a6e35103ca6d8f94423daeaca290bf94
Carlos Vazquez <carlosvazquezmurillo@gmail.com>
2017-10-29 21:22:01 -0600

    Refactor app.js

--- app/src/app.js
+++ app/src/app.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Layout extends React.Component {
+  constructor() {
+    super();
+    this.name = "John Doe";
+  }
+  getName() {
+    return this.name;
+  }
   render() {
     return(
-      <h1>Hello World!</h1>
+      <h1>Hello { this.getName() }!</h1>
       );
   }
 }
