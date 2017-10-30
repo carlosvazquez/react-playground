cae8d2bd62fa2b6f57e060f536c751a26d36f202
Carlos Vazquez <carlosvazquezmurillo@gmail.com>
2017-10-29 19:25:31 -0600

    Fix loader in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,18 @@ module.exports = {
     context: __dirname,
     devtool: debug ? "inline-sourcemap" : null,
     entry: "./app/src/app.js",
+    module: {
+        rules: [{
+            test: /\.(jsx)$/,
+            loader: 'babel-loader',
+            exclude: /node_module/,
+            query: {
+                presets: ['react', 'es2015', 'stage-0'],
+                plugins: ['react-html-attr', 'transform-class-properties', 'transform-decorators-legacy']
+            }
+          }
+        ]
+      },
     output: {
         path: __dirname + "/app/dist",
         filename: "app.js"
